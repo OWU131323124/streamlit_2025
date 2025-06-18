e20575f1ceab8a0d2a86578ce3e6affc97921592
OWU131323124 <s1323124@cst.otsuma.ac.jp>
2025-06-18 09:38:53 +0900

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,78 @@
+# member_data.py
+
+member_info = {
+    "心花りり": {
+        "color": "#F49191",
+        "image": "images/心花りり.jpeg",
+        "SNS": "https://www.instagram.com/riri_ilife/",
+        "tiktok": "https://www.tiktok.com/@kome_0519?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=J-OmNhnU3Lk",
+         "description": "三度の飯よりりりが好き",
+    },
+    "福丸うさ": {
+        "color": "#FBE981",
+        "image": "images/うさ.jpeg",
+        "SNS": "https://www.instagram.com/ilife_usa/reels/",
+        "tiktok": "https://www.tiktok.com/@fukumarusa?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=gV8xjae470U",
+        "description": "うさと笑えば福きたる"
+    },
+    "若葉のあ":{
+        "color":"#95DC8B",
+        "image": "images/のあ.jpeg",
+        "SNS" :"https://www.instagram.com/ilife_wakaba/",
+        "tiktok": "https://www.tiktok.com/@ilife__wakaba?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=0YFUVzUTBEo",
+        "description": "君に届けのあの愛"
+    },
+    "虹羽みに":{
+        "color":"#A2DDEC",
+        "image": "images/みに.jpeg",
+        "SNS" :"https://www.instagram.com/ilife.idol/related_profiles/",
+        "tiktok": "https://www.tiktok.com/@ilife_mini?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=-oYvgtgFz94",
+        "description": "みにを今日もみにきたよ"
+    },
+    "那蘭のどか":{
+        "color":"#F1B4E5",
+        "image": "images/のどか.jpeg",
+        "SNS" :"https://www.instagram.com/ilife_nodoka/",
+        "tiktok": "https://www.tiktok.com/@ilife_nodoka?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=U32RQ7dqpbg",
+        "description": "のどから声出して～！"
+    },
+    "空詩かれん":{
+        "color":"#0C54BF",
+        "image": "images/かれん.jpeg",
+        "SNS" :"https://www.instagram.com/ilife.idol/related_profiles/",
+        "tiktok": "https://www.tiktok.com/@ilife_karen?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=8NQ_nqmANVs",
+        "description": "かれんに奏でる君とソナタ"
+    },
+    "純嶺みき":{
+        "color":"#8A5EAE",
+        "image": "images/みき.jpeg",
+        "SNS" :"https://www.instagram.com/ilife.idol/related_profiles/",
+        "tiktok": "https://www.tiktok.com/@ilife_miki?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=-oYvgtgFz94",
+        "description": "すみからすみれまで全部好き"
+    },
+    "小熊まむ":{
+        "color":"#EF8215",
+        "image": "images/まむ.jpeg",
+        "SNS" :"https://www.instagram.com/ilife.idol/related_profiles/",
+        "tiktok": "https://www.tiktok.com/@ilife_koguma?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=-oYvgtgFz94",
+        "description": "まむに夢中でくまっちゃう"
+    },
+    "あいす":{
+        "color":"#999797",
+        "image": "images/あいす.jpeg",
+        "SNS" :"https://www.instagram.com/ice_icol/",
+        "tiktok": "https://www.tiktok.com/@ice_icol?is_from_webapp=1&sender_device=pc",
+        "youtube": "https://www.youtube.com/watch?v=mHhgIN3EIPg",
+        "description": "あいすのことは愛すのみ"
+    
+    },
+
+}
